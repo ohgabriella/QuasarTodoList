5e2b3cbf32480aa6ee21004b1e9a9f8bd031e7da
Gabriella Barreto <gabriella.gomes@conductor.com.br>
2019-07-18 10:04:21 -0300

    Adicionando dialog popup

---
+++ src/components/modulos/DialogPopup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn label="Confirm" color="primary" @click="confirm" />
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,22 +19,17 @@
         <q-list-header>Done</q-list-header>
         <q-item clickable v-ripple v-for="(task, index) in done" v-bind:key="task">
           <q-item-section>{{task}}</q-item-section>
-          <q-btn
-            outline
-            style="color: goldenrod;"
-            label="Excluir"
-            @click.native="deleteTask(index)"
-          />
+          <q-btn label="Excluir" color="primary" @click.native="deleteTask(index)" />
         </q-item>
       </q-list>
     </div>
   </q-page>
 </template>
-
 <style>
 </style>
 
 <script>
+
 export default {
   name: 'PageIndex',
   data () {
@@ -54,7 +49,26 @@ export default {
       this.tasks.splice(index, 1)
     },
     deleteTask (index) {
-      this.done.splice(index, 1).then(alert('Você terminou a tarefa, parabéns!'))
+      this.$q
+        .dialog({
+          title: 'Confirm',
+          message: 'Would you like to turn on the wifi?',
+          cancel: true,
+          persistent: true
+        })
+        .onOk(() => {
+          console.log('>>>> OK')
+        })
+        .onOk(() => {
+          this.done.splice(index, 1)
+          console.log('>>>> second OK catcher')
+        })
+        .onCancel(() => {
+          console.log('>>>> Cancel')
+        })
+        .onDismiss(() => {
+          console.log('I am triggered on both OK and Cancel')
+        })
     }
   }
 }
